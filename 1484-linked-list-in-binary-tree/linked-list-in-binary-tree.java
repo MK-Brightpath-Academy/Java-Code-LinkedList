ddf792a1230e0a72cec0a673693bb73595af4153
Mukesh Yadav <72705190+MK-Brightpath-Academy@users.noreply.github.com>
2025-11-07 16:00:25 +0530

    Time: 1 ms (98.92%) | Memory: 46.7 MB (11.17%) - LeetSync

---
+++ 1484-linked-list-in-binary-tree/linked-list-in-binary-tree.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSubPath(ListNode head, TreeNode root) {
+         if (root == null) return false;
+        // Check if the path starts at current node OR in left subtree OR in right subtree
+        return dfs(root, head) || isSubPath(head, root.left) || isSubPath(head, root.right);
+    }
+
+    private boolean dfs(TreeNode root, ListNode head) {
+        if (head == null) return true;   // all nodes matched
+        if (root == null) return false;  // tree path ended
+        if (root.val != head.val) return false;
+
+        // Continue checking left and right subtrees
+        return dfs(root.left, head.next) || dfs(root.right, head.next);
+    }
+}
