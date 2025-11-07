2941dce582bf1485aa0ec6c8c07c3363928bfea9
Mukesh Yadav <72705190+MK-Brightpath-Academy@users.noreply.github.com>
2025-11-07 15:56:44 +0530

    Time: 0 ms (100.00%) | Memory: 47 MB (5.42%) - LeetSync

---
+++ 109-convert-sorted-list-to-binary-search-tree/convert-sorted-list-to-binary-search-tree.java
@@ -0,0 +1,54 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode sortedListToBST(ListNode head) {
+         if (head == null) return null;
+        if (head.next == null) return new TreeNode(head.val);
+
+        ListNode prev = null;
+        ListNode slow = head;
+        ListNode fast = head;
+
+        // Find middle node
+        while (fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // Disconnect left half from the mid node
+        prev.next = null;
+
+        // slow is now the middle node
+        TreeNode root = new TreeNode(slow.val);
+
+        // Recursively build left and right subtrees
+        root.left = sortedListToBST(head);
+        root.right = sortedListToBST(slow.next);
+
+        return root;
+    }
+}
